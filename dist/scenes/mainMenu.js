e390a973afd7d71516e47bccbea02874e64c0c88
Mark Beaman <30801986+markbeaman44@users.noreply.github.com>
2025-02-25 12:06:23 +0000

    fix: improve layout and easter egg (#4)
    - Add more levels with better prompts DONE
    - Next level via click DONE
    - Next level via trying DONE
    - Make images, text response - so when screen is small they go smaller too DONE
    - Add robot animation DONE
    - Type on main menu - so 100% of the game can be completed via keyboard DONE

--- dist/scenes/mainMenu.js
+++ dist/scenes/mainMenu.js
@@ -10,13 +10,17 @@ export default class mainMenu extends Phaser.Scene {
         document.body.style.overflow = "hidden";
     }
     create() {
+        var _a, _b;
+        let fontSize = this.scale.width < 800 ? '16px' : '24px';
         // Set background
         this.cameras.main.setBackgroundColor(this.backgroundColour);
-        let title = this.add.text(-100, -300, 'Select a Level', { fontSize: '32px', color: '#ffffff' });
+        this.arrowText = this.add.text(-190, 200, ">", { fontSize: fontSize, color: "#0f0" });
+        this.inputText = this.add.text(-170, 200, "", { fontSize: fontSize, color: "#0f0" });
+        let title = this.add.text(-100, -300, 'Select Level', { fontSize: '32px', color: '#ffffff' });
         this.input.setDefaultCursor('default');
         const levelButtons = [];
         for (let i = 1; i <= constant.commands.length; i++) {
-            let levelText = this.add.text(-40, -250 + i * 50, `> Level ${i}`, { fontSize: "24px", color: "#0f0" })
+            let levelText = this.add.text(-40, -250 + i * 50, `> Level ${i}`, { fontSize: fontSize, color: "#0f0" })
                 .setInteractive()
                 .on("pointerover", () => this.input.setDefaultCursor("pointer"))
                 .on("pointerout", () => this.input.setDefaultCursor("default"))
@@ -24,11 +28,30 @@ export default class mainMenu extends Phaser.Scene {
             levelButtons.push(levelText);
         }
         // Create a container for all elements
-        this.levelGroup = this.add.container(this.scale.width / 2, this.scale.height / 2, [title, ...levelButtons]);
+        this.levelGroup = this.add.container(this.scale.width / 2, this.scale.height / 2, [title, this.inputText, this.arrowText, ...levelButtons]);
         this.scale.on("resize", this.resizeGame, this);
+        // Handle keyboard input
+        (_b = (_a = this.input) === null || _a === void 0 ? void 0 : _a.keyboard) === null || _b === void 0 ? void 0 : _b.on('keydown', (event) => {
+            if (event.key === "Enter") {
+                let typedCommand = this.inputText.text.trim().toLowerCase();
+                let levelNumber = Number(typedCommand.split('level ')[1]);
+                if (typedCommand === `level ${levelNumber}` && levelNumber <= constant.commands.length) {
+                    this.scene.start(`level${levelNumber}`);
+                }
+                this.inputText.setText("");
+            }
+            else if (event.key === "Backspace") {
+                this.inputText.text = this.inputText.text.slice(0, -1);
+            }
+            else if (event.key.length === 1) {
+                this.inputText.text += event.key;
+            }
+        });
     }
     resizeGame(gameSize) {
         let { width, height } = gameSize;
+        let scaleFactor = width < 800 ? 0.9 : 1;
+        this.levelGroup.setScale(scaleFactor);
         // Ensure camera covers full size
         this.cameras.resize(width, height);
         // Reposition Images within Group
--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -6,6 +6,8 @@ export default class mainMenu extends Phaser.Scene {
     }
     public backgroundColour: string = "#224";
     public levelGroup!: Phaser.GameObjects.Container;
+    public inputText!: Phaser.GameObjects.Text;
+    public arrowText!: Phaser.GameObjects.Text;
 
     preload() {
         document.body.style.margin = "0";
@@ -14,16 +16,21 @@ export default class mainMenu extends Phaser.Scene {
     }
 
     create() {
+        let fontSize = this.scale.width < 800 ? '16px' : '24px';
+
         // Set background
         this.cameras.main.setBackgroundColor(this.backgroundColour);
+
+        this.arrowText = this.add.text(-190, 200, ">", { fontSize: fontSize, color: "#0f0" });
+        this.inputText = this.add.text(-170, 200, "", { fontSize: fontSize, color: "#0f0" });
         
-        let title = this.add.text(-100, -300, 'Select a Level', { fontSize: '32px', color: '#ffffff' });
+        let title = this.add.text(-100, -300, 'Select Level', { fontSize: '32px', color: '#ffffff' });
         this.input.setDefaultCursor('default');
 
         const levelButtons: any[] = []
-
+    
         for (let i = 1; i <= constant.commands.length; i++) {
-            let levelText = this.add.text(-40, -250 + i * 50, `> Level ${i}`, { fontSize: "24px", color: "#0f0" })
+            let levelText = this.add.text(-40, -250 + i * 50, `> Level ${i}`, { fontSize: fontSize, color: "#0f0" })
                 .setInteractive()
                 .on("pointerover", () => this.input.setDefaultCursor("pointer"))
                 .on("pointerout", () => this.input.setDefaultCursor("default"))
@@ -33,13 +40,34 @@ export default class mainMenu extends Phaser.Scene {
         }
 
         // Create a container for all elements
-        this.levelGroup = this.add.container(this.scale.width / 2, this.scale.height / 2, [title, ...levelButtons as any])
+        this.levelGroup = this.add.container(this.scale.width / 2, this.scale.height / 2,
+            [title, this.inputText, this.arrowText, ...levelButtons as any]
+        )
 
         this.scale.on("resize", this.resizeGame, this);
+
+        // Handle keyboard input
+        this.input?.keyboard?.on('keydown', (event) => {
+            if (event.key === "Enter") {
+                let typedCommand = this.inputText.text.trim().toLowerCase();
+                let levelNumber = Number(typedCommand.split('level ')[1]);
+                if (typedCommand === `level ${levelNumber}` && levelNumber <= constant.commands.length) {
+                    this.scene.start(`level${levelNumber}`);
+                }
+                this.inputText.setText("");
+            } else if (event.key === "Backspace") {
+                this.inputText.text = this.inputText.text.slice(0, -1);
+            } else if (event.key.length === 1) {
+                this.inputText.text += event.key;
+            }
+        });
     }
 
     resizeGame(gameSize) {
         let { width, height } = gameSize;
+
+        let scaleFactor = width < 800 ? 0.9 : 1;
+        this.levelGroup.setScale(scaleFactor);
       
         // Ensure camera covers full size
         this.cameras.resize(width, height);
